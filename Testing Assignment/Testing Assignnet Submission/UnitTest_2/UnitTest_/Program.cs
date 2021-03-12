632a9f08b2b7cf6c19041e2f85dcf2cadf2c763d
Hardi Mehta <hardi.mehta@gatewaygroup.com>
2021-03-12 19:07:35 +0530

    Testing 2nd assignment

---
+++ Testing Assignment/Testing Assignnet Submission/UnitTest_2/UnitTest_/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace UnitTest_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = "Hardi";
+            string num = "2012H";
+            if (num.IsValidNumeric() == true)
+                Console.WriteLine("\"" + num + "\" is not valid numeric value");
+            else
+                Console.WriteLine("\"" + num + "\" is not valid numeric value");
+            Console.WriteLine("##==##==##==##==##==##==##==##==##==##==##==##==##==##==##==##==##==##==");
+            if (num.ConvertStringToNumber() == null)
+                Console.WriteLine("\"" + num + "\" can not convert in numeric value");
+            else
+                Console.WriteLine("\"" + num + "\" numeric value is : " + num.ConvertStringToNumber());
+            Console.WriteLine("Input : \"" + input + "\" into Uppercase Letter : " + input.ConvertToUpper());
+            input = "HARDI";
+            Console.WriteLine("Input : \"" + input + "\" into Lowercase Letter : " + input.ConvertToLower());
+            input = "hardi mehta";
+            Console.WriteLine("Input : \"" + input + "\" into Titlecase Letter : " + input.ConvertToTitleCase());
+            input = "hardi";
+            Console.WriteLine("Check all the character of string is lower case or not : \"" + input + "\" : " + input.IsLower());
+            input = "hardi mehta";
+            Console.WriteLine("Input : \"" + input + "\" into Capitalize : " + input.ConvertToCapitalize());
+            input = "HARDI";
+            Console.WriteLine("Is input in uppercase : \"" + input + "\" : " + input.IsUpper());
+            input = "hardi mehta";
+            Console.WriteLine("No of words in the input : \"" + input + "\" : " + input.CountingWord());
+            input = "hardi mehta";
+            Console.WriteLine("Removing last character from input : \"" + input + "\" : "+ input.RemoveLastCharacterFrom());            
+            Console.WriteLine("##==##==##==##==##==##==##==##==##==##==##==##==##==##==##==##==##==##==");
+        }
+    }
+}
